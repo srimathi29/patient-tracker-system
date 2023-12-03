78aa5e4795ced76a5a487f56455a58a88754ae67
srimathi29 <srimathi12345@yahoo.co.in>
2023-12-02 19:40:38 -0500

    Initial implementation for Patient and Doctor class

--- Backend/Models/models.py
+++ Backend/Models/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     '''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
     #password = db.Column(db.String(80), nullable=False)
     #email = db.Column(db.String(120), unique=True, nullable=False)
     first_name = db.Column(db.String(80), nullable=False)
@@ -15,4 +16,21 @@ class User(db.Model):
     # is_superuser = db.Column(db.Boolean, default=False)
     last_login = db.Column(db.DateTime, nullable=True)
     date_joined = db.Column(db.DateTime, nullable=False)
-    
+
+class Patient(db.Model):
+    '''
+    Patient model for the database.
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    gender = db.Column(db.String(10), nullable=True)
+    contact_number = db.Column(db.String(15), nullable=True)
+    address = db.Column(db.String(200), nullable=True)
+
+class Doctor(db.Model):
+    '''
+    Doctor model for the database.
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    credentials = db.Column(db.String(100), nullable=False)
