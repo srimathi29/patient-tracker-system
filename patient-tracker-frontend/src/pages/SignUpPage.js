e3218a89ddd9913a0fc9b05eb33aefdfb841e84d
Satyasriram <satyasp.edu2023@gmail.com>
2023-12-15 00:00:20 -0500

    Integrating Signup form with Backend

--- patient-tracker-frontend/src/pages/SignUpPage.js
+++ patient-tracker-frontend/src/pages/SignUpPage.js
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'; // Import Axios
 import Card from '../components/ui/Card';
 import classes from './SignUpPage.module.css';
 
@@ -13,7 +14,7 @@ function SignUpPage() {
         role: ''
     });
 
-    const navigate = useNavigate(); // Get the navigate function for navigation
+    const navigate = useNavigate();
 
     const usernameRef = useRef();
     const passwordRef = useRef();
@@ -23,6 +24,7 @@ function SignUpPage() {
     const roleRef = useRef();
     const [isSuccess, setIsSuccess] = useState(false);
 
+    // Validation functions
     function validateUsername(enteredUsername) {
         return /^[A-Za-z0-9]+$/.test(enteredUsername) ? '' : 'Username must not contain spaces or special characters.';
     }
@@ -56,13 +58,29 @@ function SignUpPage() {
     function handleSubmit(event) {
         event.preventDefault();
         if (validateInput()) {
-            // Simulate a successful sign-up
-            // Replace this with your actual sign-up logic
-            setTimeout(() => {
-                setIsSuccess(true);
-                // Redirect to the sign-in page using navigate
-                navigate('/login'); // Navigate to the sign-in page
-            }, 2000);
+            const userData = {
+                username: usernameRef.current.value,
+                user_email: emailRef.current.value,
+                password: passwordRef.current.value,
+                first_name: firstNameRef.current.value,
+                last_name: lastNameRef.current.value,
+                user_type: roleRef.current.value
+            };
+
+            axios.post('http://10.0.0.123:8080/register', JSON.stringify(userData), {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    console.log(response.data);
+                    setIsSuccess(true);
+                    navigate('/login'); // Navigate to the sign-in page
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    // Handle errors here, e.g., set error messages in state
+                });
         }
     }
 
@@ -103,70 +121,35 @@ function SignUpPage() {
     return (
         <Card>
             <form className={classes.form} onSubmit={handleSubmit}>
-
+                {/* Input fields for username, password, email, first name, last name, role */}
                 <div className={classes.control}>
                     <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        required
-                        id="username"
-                        ref={usernameRef}
-                        onChange={(event) => handleInputChange(event, validateUsername, 'username')}
-                    />
+                    <input type="text" required id="username" ref={usernameRef} onChange={(event) => handleInputChange(event, validateUsername, 'username')} />
                     {errors.username && <p className={classes.error}>{errors.username}</p>}
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        required
-                        id="password"
-                        ref={passwordRef}
-                        onChange={(event) => handleInputChange(event, validatePassword, 'password')}
-                    />
+                    <input type="password" required id="password" ref={passwordRef} onChange={(event) => handleInputChange(event, validatePassword, 'password')} />
                     {errors.password && <p className={classes.error}>{errors.password}</p>}
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        required
-                        id="email"
-                        ref={emailRef}
-                        onChange={(event) => handleInputChange(event, validateEmail, 'email')}
-                    />
+                    <input type="email" required id="email" ref={emailRef} onChange={(event) => handleInputChange(event, validateEmail, 'email')} />
                     {errors.email && <p className={classes.error}>{errors.email}</p>}
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="firstName">First Name</label>
-                    <input
-                        type="text"
-                        required
-                        id="firstName"
-                        ref={firstNameRef}
-                        onChange={(event) => handleInputChange(event, validateFirstName, 'firstName')}
-                    />
+                    <input type="text" required id="firstName" ref={firstNameRef} onChange={(event) => handleInputChange(event, validateFirstName, 'firstName')} />
                     {errors.firstName && <p className={classes.error}>{errors.firstName}</p>}
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="lastName">Last Name</label>
-                    <input
-                        type="text"
-                        required
-                        id="lastName"
-                        ref={lastNameRef}
-                        onChange={(event) => handleInputChange(event, validateLastName, 'lastName')}
-                    />
+                    <input type="text" required id="lastName" ref={lastNameRef} onChange={(event) => handleInputChange(event, validateLastName, 'lastName')} />
                     {errors.lastName && <p className={classes.error}>{errors.lastName}</p>}
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="role">Role</label>
-                    <select
-                        id="role"
-                        ref={roleRef}
-                        required
-                        onChange={(event) => handleInputChange(event, validateRole, 'role')}
-                    >
+                    <select id="role" ref={roleRef} required onChange={(event) => handleInputChange(event, validateRole, 'role')}>
                         <option value="">Select a role</option>
                         <option value="Admin">Admin</option>
                         <option value="Doctor">Doctor</option>
