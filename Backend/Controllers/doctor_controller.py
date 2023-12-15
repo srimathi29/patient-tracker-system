4f04fa55d3ad40d551b9d694698b56e19adb5d2a
Prudhvi <snikku@umass.edu>
2023-12-15 01:45:08 -0500

    Adding Controllers

---
+++ Backend/Controllers/doctor_controller.py
@@ -0,0 +1,113 @@
+from flask import request, jsonify
+from flask_restful import Resource
+from ..Models.models import db, Doctor, User
+
+from flask import Response, json
+
+class DoctorAPI(Resource):
+    def get(self, user_id=None):
+        if user_id:
+            # Get a specific doctor by user_id
+            doctor = Doctor.query.filter_by(user_id=user_id).first()
+            if doctor:
+                response_data = {
+                    "data": {
+                        "doctor": doctor.serialize()
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=200)
+            response_data = {
+                "data": {
+                    "message": "Doctor not found"
+                }
+            }
+            return Response(json.dumps(response_data), mimetype="application/json", status=404)
+        else:
+            # Get all doctors
+            doctors = Doctor.query.all()
+            serialized_doctors = [doctor.serialize() for doctor in doctors]
+            # print(serialized_doctors)
+            response_data = {
+                "data": serialized_doctors
+            }
+            # print(response_data)
+            return Response(json.dumps(response_data), mimetype="application/json", status=200)
+
+    def post(self):
+        data = request.json
+        user = User.query.get(data['user_id'])
+        if user and not Doctor.query.filter_by(user_id=user.id).first():
+            try:
+                new_doctor = Doctor(
+                    user_id=user.id,
+                    specialization=data.get('specialization', 'general'),
+                    license_start_date=data.get('license_start_date'),
+                    last_license_renewed=data.get('last_license_renewed'),
+                    license_expiry_date=data.get('license_expiry_date'),
+                    license_number=data.get('license_number')
+                )
+                db.session.add(new_doctor)
+                db.session.commit()
+                response_data = {
+                    "data": {
+                        "message": "Doctor profile created successfully"
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=201)
+            except Exception as e:
+                db.session.rollback()
+                response_data = {
+                    "data": {
+                        "message": str(e)
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=400)
+        response_data = {
+            "data": {
+                "message": "User already has a doctor profile or does not exist"
+            }
+        }
+        return Response(json.dumps(response_data), mimetype="application/json", status=409)
+
+    def put(self, user_id):
+        doctor = Doctor.query.filter_by(user_id=user_id).first()
+        if doctor:
+            data = request.json
+            doctor.specialization = data.get('specialization', doctor.specialization)
+            doctor.license_start_date = data.get('license_start_date', doctor.license_start_date)
+            doctor.last_license_renewed = data.get('last_license_renewed', doctor.last_license_renewed)
+            doctor.license_expiry_date = data.get('license_expiry_date', doctor.license_expiry_date)
+            doctor.license_number = data.get('license_number', doctor.license_number)
+
+            db.session.commit()
+            response_data = {
+                "data": {
+                    "message": "Doctor profile updated successfully"
+                }
+            }
+            return Response(json.dumps(response_data), mimetype="application/json", status=200)
+        response_data = {
+            "data": {
+                "message": "Doctor not found"
+            }
+        }
+        return Response(json.dumps(response_data), mimetype="application/json", status=404)
+
+    def delete(self, user_id):
+        doctor = Doctor.query.filter_by(user_id=user_id).first()
+        if doctor:
+            db.session.delete(doctor)
+            db.session.commit()
+            response_data = {
+                "data": {
+                    "message": "Doctor profile deleted successfully"
+                }
+            }
+            return Response(json.dumps(response_data), mimetype="application/json", status=200)
+        response_data = {
+            "data": {
+                "message": "Doctor not found"
+            }
+        }
+        return Response(json.dumps(response_data), mimetype="application/json", status=404)
+
---
+++ Backend/Controllers/patient_controller.py
@@ -0,0 +1,120 @@
+from flask import request, jsonify, Response, json
+from flask_restful import Resource, Api
+from ..Models.models import db, Patient, User
+
+class PatientAPI(Resource):
+    def get(self, user_id=None):
+        if user_id:
+            # Get a specific patient by user_id
+            patient = Patient.query.filter_by(user_id=user_id).first()
+            if patient:
+                response_data = {
+                    "data": {
+                        "patient": patient.serialize()
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=200)
+            response_data = {
+                "data": {
+                    "message": "Patient not found"
+                }
+            }
+            return Response(json.dumps(response_data), mimetype="application/json", status=404)
+        else:
+            # Get all patients
+            patients = Patient.query.all()
+            serialized_patients = [patient.serialize() for patient in patients]
+            response_data = {
+                "data": {
+                    "patients": serialized_patients
+                }
+            }
+            return Response(json.dumps(response_data), mimetype="application/json", status=200)
+
+    def post(self):
+        data = request.json
+        user = User.query.get(data['user_id'])
+        if user and not Patient.query.filter_by(user_id=user.id).first():
+            try:
+                new_patient = Patient(
+                    user_id=user.id,
+                    # Add other fields from data, if necessary...
+                )
+                db.session.add(new_patient)
+                db.session.commit()
+                response_data = {
+                    "data": {
+                        "message": "Patient profile created successfully"
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=201)
+            except Exception as e:
+                db.session.rollback()
+                response_data = {
+                    "data": {
+                        "message": str(e)
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=400)
+        response_data = {
+            "data": {
+                "message": "User already has a patient profile or does not exist"
+            }
+        }
+        return Response(json.dumps(response_data), mimetype="application/json", status=409)
+
+    def put(self, user_id):
+        patient = Patient.query.filter_by(user_id=user_id).first()
+        if patient:
+            try:
+                data = request.json
+                # Update fields from data, if necessary...
+                db.session.commit()
+                response_data = {
+                    "data": {
+                        "message": "Patient profile updated successfully"
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=200)
+            except Exception as e:
+                db.session.rollback()
+                response_data = {
+                    "data": {
+                        "message": str(e)
+                    }
+                }
+                return Response(json.dumps(response_data), mimetype="application/json", status=400)
+        response_data = {
+            "data": {
+                "message": "Patient not found"
+            }
+        }
+        return Response(json.dumps(response_data), mimetype="application/json", status=404)
+
+    def delete(self, user_id):
+        patient = Patient.query.filter_by(user_id=user_id).first()
+        if patient:
+            db.session.delete(patient)
+            db.session.commit()
+            response_data = {
+                "data": {
+                    "message": "Patient profile deleted successfully"
+                }
+            }
+            return Response(json.dumps(response_data), mimetype="application/json", status=200)
+        response_data = {
+            "data": {
+                "message": "Patient not found"
+            }
+        }
+        return Response(json.dumps(response_data), mimetype="application/json", status=404)
+
+# Method to serialize Patient data
+# def serialize_patient(patient):
+#     return {
+#         "user_id": patient.user_id,
+#         # Add other serialization fields here...
+#     }
+
+# Patient.serialize = serialize_patient
+
